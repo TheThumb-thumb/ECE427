3a00fc5d2ce949028ebed0e47daec3ee55ffba92
TheThumb-thumb <90225967+TheThumb-thumb@users.noreply.github.com>
2025-07-08 13:28:58 -0500

    Update types.sv

--- types.sv
+++ types.sv
@@ -2,7 +2,7 @@ package params
     localparam IV_WIDTH = 80;
     localparam SETUP_TIME = 1151; // Cycles needed for trivium to cycle through internal state 4 times = 4 x 288 = 1152
     localparam ADC_FIF0_DEPTH = 80;
-    localparam KEY_WIDTH = 1024;
+    localparam KEY_WIDTH = 1024;    
 
 endpackage
 
@@ -10,7 +10,7 @@ package le_types
 import params::*;
 
     typedef enum logic [1:0] {
-        IDL = 2'b00, 
+        IDLE = 2'b00, 
         IV_GEN = 2'b01, 
         SETUP = 2'b11, 
         GEN = 2'b10
