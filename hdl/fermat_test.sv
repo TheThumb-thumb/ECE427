7ca5efce80f495f2586cebb2e8c9a8bf4029041c
Rohan <rughani3@illinois.edu>
2025-07-26 19:28:16 -0500

    folders made

--- hdl/fermat_test.sv
+++ hdl/fermat_test.sv
@@ -21,6 +21,8 @@ logic [31:0] a;
 
 assign a = p[1] ? q[255:223] : p[255:223]; // randomly assign an a based on bits of p and q
 
+// test
+
 always_comb begin : primailty_state
     
     unique case ()
